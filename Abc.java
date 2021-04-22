28dbf9dc19b694b92b6fa1f757ce93db0046a89b
RellaRajasri <82994758+RellaRajasri@users.noreply.github.com>
2021-04-22 15:42:20 +0530

    Create Abc.java

---
+++ Abc.java
@@ -0,0 +1,11 @@
+public class Abc
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<5;i++){
+		System.out.println("my abc project");
+
+		}
+		Sytem.out.println("random line");
+	}
+}
